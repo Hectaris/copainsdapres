2015cc8e9ffed584fbcf98ae8fea05bad8d8c949
Hectaris <hectaris.supp@gmail.com>
2021-05-06 13:00:12 +0200

    v0.01

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,8 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-    
-    @user = current_user
 
-    @post.set_user!(current_user)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,4 @@
 class Post < ApplicationRecord
     belongs_to :users
     has_many :comments
-
-    def set_user!(user)
-        self.user_id = user.id
-    
-        self.save!
-      end
 end
